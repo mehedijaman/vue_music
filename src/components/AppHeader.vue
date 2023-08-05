7f217cea65bde4add6aab5767cec54be9d14cf39
Mehedi Jaman <mail4mjaman@gmail.com>
2023-08-05 09:28:50 +0600

    Home Page setup done

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -1,8 +1,10 @@
-<script></script>
+<script setup>
+import { RouterLink } from 'vue-router';
+</script>
 <template>
 <header id="header" class="bg-gray-700">
     <nav class="container mx-auto flex justify-start items-center py-5 px-4">
-        <a class="text-white font-bold uppercase text-2xl mr-4" href="#">Music</a>
+        <RouterLink class="text-white font-bold uppercase text-2xl mr-4" to="/">Music</RouterLink>
 
         <div class="flex flex-grow items-center">
             <ul class="flex flex-row mt-1">
--- src/views/NotFound.vue
+++ src/views/NotFound.vue
@@ -1,6 +1,6 @@
 <script setup></script>
 <template>
     <div>
-        <h1 class="text-3xl text-center text-gray-800">Error 404 ! Not Found.</h1>
+        <h1 class="text-3xl text-center text-gray-800 mt-5">Error 404 ! Not Found.</h1>
     </div>
 </template>
